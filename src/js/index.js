84ba457e32a5575ec8c15c78e85b6e8822679561
Rodrigo Borges <digodesign94@gmail.com>
2022-12-31 18:07:42 -0300

    Feature: added script to crypting and decrypting

---
+++ src/js/index.js
@@ -0,0 +1,30 @@
+var textInput = document.querySelector("#textToDecode")
+
+function crypt () {
+    var text = textInput.value;
+
+    var cryptedText = text.replace(/a/i, "xal@d").replace(/i/s, "w%alt1b.").replace(/o/ , "br 1&w&")
+    .replace(/c/i, "df d").replace(/e/d, "trx f").replace(/u/g, " gd").replace(/ss/i, "f jf5")
+    
+
+    document.getElementById('searcherscream').innerHTML = '<textarea  readonly class="newText" >' + cryptedText + 
+    '</textarea>' + '<button class="btnCopy" id="copy" onclick="copy()">Copiar</button>'
+}
+
+function decrypt () {
+    var text = textInput.value;
+
+    var decryptedText = text.replace(/xal@d/i, "a").replace(/w%alt1b./s, "i").replace(/br 1&w&/ , "o")
+    .replace(/df d/i, "c").replace(/trx f/d, "e").replace(/ gd/g, "u").replace(/f jf5/i, "ss")
+
+    document.getElementById('searcherscream').innerHTML = '<textarea readonly class="newText" >' + decryptedText + 
+    '</textarea>' + '<button class="btnCopy" id="copy" onclick="copy()">Copiar</button>'
+}
+
+function copy() {
+    var textoCop = document.getElementById('textToDecode');
+  
+    textoCop.select();
+    document.execCommand('copy');
+    alert("Texto copiado.");
+} 
